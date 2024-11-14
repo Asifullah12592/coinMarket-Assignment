8ce85e7bc9e87dec9ba4c419a9bf659d28fa6e96
Asifullah12592 <Asifullah12592@gmail.com>
2024-11-14 19:09:04 +0500

    coin market assignment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
 
-export default nextConfig;
+// next.config.js
+module.exports = {
+  eslint: {
+    ignoreDuringBuilds: true, // Build ke waqt ESLint errors ignore karega
+  },
+}
+export default module.exports;
